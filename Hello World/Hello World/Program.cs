64eacccaabc1fc1488250d95e105fbe7f0144067
TrishDeLozier <trishdelozier@gmail.com>
2014-06-26 11:02:59 -0400

    Create and Develop C#
    Create Hello World Program

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string name = "My name is Trish";
+            Console.WriteLine(name);
+            Console.WriteLine("Write me a message: ");
+            string inputReadLine;
+            inputReadLine = Console.ReadLine();
+            Console.WriteLine("This is what you typed: " + inputReadLine);
+            
+      
+            
+            Console.Read();
+            
+        }
+    }
+}
